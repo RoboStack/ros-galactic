a42688a7a7ec184e1505bde920270d1625e9af5d
Tobias Fischer <info@tobiasfischer.info>
2021-07-30 17:42:25 +1000

    Fix channel defaults

--- .scripts/build_linux.sh
+++ .scripts/build_linux.sh
@@ -26,6 +26,7 @@ conda config --set remote_max_retries 5
 # conda config --append channels defaults
 conda config --add channels conda-forge
 conda config --add channels robostack-experimental
+conda config --remove channels defaults
 conda config --set channel_priority strict
 
 conda update conda -c conda-forge
